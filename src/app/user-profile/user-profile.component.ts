81c5e97d8be8e45f30ec57897a9b42a22c3673d5
Jérémy GUIMARD <jeremy.guimard.pro@gmail.com>
2018-10-15 21:27:21 +0200

    Answers

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+  User = {
+    name : 'Kakarot',
+    firstName : 'Goku',
+    age : 40,
+    quote : 'Les limites existent uniquement si tu le permets',
+    photo : 'http://image.noelshack.com/fichiers/2018/42/1/1539629145-supersayajin3-f6f919a01468cbc6c36c3aab9ab9b06473aad00c.jpeg'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
